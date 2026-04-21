fb7f5c75c2519b2d86c93dd1fc99586f3817e804
BRONX-09 <manubhavsharma09@gmail.com>
2026-04-21 21:51:15 +0530

    changes

--- README.md
+++ README.md
@@ -1,17 +1,3 @@
-# Folder structure
-
-- `src` - source code for your kaplay project
-- `dist` - distribution folder, contains your index.html, built js bundle and static assets
-
-
-## Development
-
-```sh
-$ npm run dev
-```
-
-will start a dev server at http://localhost:8000
-
 ## Distribution
 
 ```sh
--- src/entities/player.js
+++ src/entities/player.js
@@ -8,7 +8,7 @@ export function makePlayer(k) {
     k.sprite("adventurer"),
     k.area({ shape: new k.Rect(k.vec2(0, 18), 12, 12) }),
     k.anchor("center"),
-    k.body({ mass: 100, jumpForce: 320 }),
+    k.body({ mass: 100, jumpForce: 400 }),
     k.doubleJump(state.current().isDoubleJumpUnlocked ? 2 : 1),
     k.opacity(),
     k.health(state.current().playerHp),
